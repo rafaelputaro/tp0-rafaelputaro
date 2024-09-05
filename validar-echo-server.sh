748b840d21a73f66527d89fe9b118340e9274828
rafael <rafaelputaro@gmail.com>
2024-09-05 11:32:58 -0300

    upgrade validad

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -6,11 +6,11 @@ OK_MESSAGE="action: test_echo_server | result: success"
 FAIL_MESSAGE="action: test_echo_server | result: fail"
 MESSAGE_TO_SERVER="Hello Server"
 
-RESPONSE=$(echo "$MESSAGE_TO_SERVER" | nc -w 3 $URL $PORT)
+RESPONSE=$(echo "$MESSAGE_TO_SERVER" | nc -w 3 $URL_ALT $PORT)
 if [ "$RESPONSE" = "$MESSAGE_TO_SERVER" ]; then
   echo "$OK_MESSAGE"
 else
-  RESPONSE=$(echo "$MESSAGE_TO_SERVER" | nc -w 3 $URL_ALT $PORT)
+  RESPONSE=$(echo "$MESSAGE_TO_SERVER" | nc -w 3 $URL $PORT)
   if [ "$RESPONSE" = "$MESSAGE_TO_SERVER" ]; then
     echo "$OK_MESSAGE"
   else
