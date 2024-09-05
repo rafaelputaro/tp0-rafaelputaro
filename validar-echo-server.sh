52e2f96883a16c668913018a2dbbf4d06b68d61f
rafaelputaro <rafaelputaro@gmail.com>
2024-09-05 16:43:25 -0300

    upgrade validar

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -5,8 +5,8 @@ OK_MESSAGE="action: test_echo_server | result: success"
 FAIL_MESSAGE="action: test_echo_server | result: fail"
 MESSAGE_TO_SERVER="Hello Server"
 
-#RESPONSE=$(echo "$MESSAGE_TO_SERVER" | nc -w 3 $URL $PORT)
-RESPONSE=$(echo "$MESSAGE_TO_SERVER" | nc "server:12345")
+RESPONSE=$(echo "$MESSAGE_TO_SERVER" | nc -w 3 $URL $PORT)
+#RESPONSE=$(echo "$MESSAGE_TO_SERVER" | nc "server:12345")
 
 if [ "$RESPONSE" = "$MESSAGE_TO_SERVER" ]; then
   echo "$OK_MESSAGE"
